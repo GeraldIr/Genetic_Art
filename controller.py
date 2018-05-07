3a8026c90c2a76f97307da78ea2b4702028ce981
GeraldIr <gerald.irsiegler@gmail.com>
2018-05-07 13:25:40 +0200

    some changes

--- controller.py
+++ controller.py
@@ -13,13 +13,6 @@ def get_next_candidate_visual():
     return -1
 
 
-def test():
-    value = generation.get_pixel_value(32, 32)
-    print(int(value[0][0] * 255), int(value[1][0] * 255), int(value[2][0] * 255))
-    value = generation.get_pixel_value(63, 63)
-    print(int(value[0][0] * 255), int(value[1][0] * 255), int(value[2][0] * 255))
-
-
 def generate_visual(individual):
     im = Image.new("RGB", values.picture_size, color=0)
     pixels = im.load()
@@ -29,6 +22,7 @@ def generate_visual(individual):
             pixels[x, y] = (int(value[0][-1]*255), int(value[1][-1]*255), int(value[2][-1]*255))
 
     im.save("hello.jpg", "JPEG")
+    im.show("Picture")
     return -1
 
 
@@ -43,7 +37,8 @@ def generate_visual_fp_bw(individual):
             print(int(value[index]*255))
             pixels[x, y] = int(value[index]*255)
 
-    im.save("hello.jpg", "JPEG")
+    im.save("Picture.jpg", "JPEG")
+    im.show("Picture")
     return -1
 
 
@@ -54,17 +49,31 @@ def generate_visual_fp(individual):
 
     for x in range(values.picture_size_x):
         for y in range(values.picture_size_y):
-            ir = (values.picture_size_x * x + y)
-            ig = (values.picture_size_x * x + y)+(values.picture_size_x*values.picture_size_y)
-            ib = (values.picture_size_x * x + y)+(2*values.picture_size_x*values.picture_size_y)
+            #ir = (values.picture_size_x * x + y)
+            #ig = (values.picture_size_x * x + y)+(values.picture_size_x*values.picture_size_y)
+            #ib = (values.picture_size_x * x + y)+(2*values.picture_size_x*values.picture_size_y)
 
-            print(int(value[ir]*255))
+            ir = (values.picture_size_x * x + y)*3
+            ig = ((values.picture_size_x * x + y)*3)+1
+            ib = ((values.picture_size_x * x + y)*3)+2
+
+            print(str(int(value[ir]*255)) + ", " + str(int(value[ig]*255)) + ", " + str(int(value[ib]*255)))
             pixels[x, y] = (int(value[ir]*255), int(value[ig]*255), int(value[ib]*255))
 
-
-    im.save("hello.jpg", "JPEG")
+    im.save("Picture.jpg", "JPEG")
+    im.show("Picture")
     return -1
 
+
+def test():
+    cmd = input("Full Picture (fp), or Pixel Based Neural Network (pb)?")
+    if cmd == "fp":
+        generate_visual_fp(1)
+    elif cmd == "pb":
+        generate_visual(1)
+    else:
+        print("Exiting.")
+
 # generate_visual(1)
-generate_visual_fp(1)
-# test()
+# generate_visual_fp(1)
+test()
